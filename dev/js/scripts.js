2c4476f1c513d5160692f41c2b7547cc72f63297
Valentina Angelkova <vva27@drexel.edu>
2022-05-15 21:11:43 -0400

    added svg

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,9 @@
-// import { gsap } from "gsap";
+ import { gsap } from "gsap";
+
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { GSDevTools } from "gsap/GSDevTools";
+import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
+import { MotionPathHelper } from "gsap/MotionPathHelper";
+gsap.registerPlugin(MotionPathPlugin, DrawSVGPlugin, GSDevTools, MorphSVGPlugin, MotionPathHelper);
+
+const MainTL = gsap.timeline();
