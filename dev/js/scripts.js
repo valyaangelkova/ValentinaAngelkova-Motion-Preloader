4f192bc56f264cc008060c25d91eb2b1f40ffc84
Valentina Angelkova <vva27@drexel.edu>
2022-05-17 20:54:58 -0400

    paths are drawing simultaneously

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -15,27 +15,31 @@ const MainTL = gsap.timeline();
 //.set ("#Ellipse2"), {alpha:0};
 //}
 
-function DrawLine(){
-    let tl = gsap.timeline();
-tl.from("#VectorEnd1", {duration:0.5, alpha:0},1)
-.from("#Vector3", {duration:2, drawSVG:0},1)
-.from("#VectorEnd2", {duration:0.5, alpha:0},3)
-;
-return tl;
-}
+//function DrawLine(){
+//    let tl = gsap.timeline();
+//tl.from("#VectorEnd1", {duration:0.5, alpha:0},1)
+//.from("#Vector3", {duration:2, drawSVG:0},1)
+//.from("#VectorEnd2", {duration:0.5, alpha:0},3)
+//;
+//return tl;
+//}
 
 function DrawCircles(){
     let tl = gsap.timeline();
-    tl.to("#Ellipse3", {duration:2, drawSVG:0})
-    .from ("#cirlce2", {duration:1, scale:0})
-    .from("#button", {duration:2, y:-100, alpha:0, ease:"bounce.out"})
+    tl.from("#VectorEnd1", {duration:0.25, alpha:0, ease:"none"})
+.from("#Vector3", {duration:2.5, drawSVG:0, ease:"none"},"label1")
+.from("#VectorEnd2", {duration:0.25, alpha:0, ease:"none"},"+=0.25")
+
+    .to("#Ellipse3", {duration:2, drawSVG:0}, "label1")
+    .from ("#cirlce2", {duration:1, scale:0}, "label")
+    .from("#button", {duration:2, y:-100, alpha:0, ease:"bounce.out"},"label")
 ;
     return tl;
 }
 
 function Shutters(){
    let tl=gsap.timeline();
-   tl.to("#shutter1",{duration:1, fill:"#464846", x:"+=50", y:"+=70"})
+   tl.to("#shutter1",{duration:1, fill:"#464846", x:"-=50", y:"-=70"})
    .to("#shutter2",{duration:1, fill:"#464846", x:725, y:586})
    ;
    return tl;
@@ -44,7 +48,7 @@ function Shutters(){
 
 
 MainTL
-.add(DrawLine())
+//.add(DrawLine())
 .add(DrawCircles())
 .add(Shutters())
 ;
