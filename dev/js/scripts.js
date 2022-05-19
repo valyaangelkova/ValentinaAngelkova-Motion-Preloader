3e79c1d7e34e032653c3f4fd1bc0bfae62fbaedd
Valentina Angelkova <vva27@drexel.edu>
2022-05-19 17:45:54 -0400

    animating

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -3,8 +3,12 @@ import { gsap } from "gsap";
 import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
 import { GSDevTools } from "gsap/GSDevTools";
 import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
+
+import { CustomEase } from "gsap/CustomEase";
+import { CustomWiggle } from "gsap/CustomWiggle";
+
 //import { MotionPathHelper } from "gsap/MotionPathHelper";
-gsap.registerPlugin(DrawSVGPlugin, GSDevTools, MorphSVGPlugin );
+gsap.registerPlugin(DrawSVGPlugin, GSDevTools, MorphSVGPlugin, CustomEase, CustomWiggle );
 
 const MainTL = gsap.timeline();
 
@@ -15,45 +19,70 @@ const MainTL = gsap.timeline();
 //.set ("#Ellipse2"), {alpha:0};
 //}
 
-//function DrawLine(){
-//    let tl = gsap.timeline();
-//tl.from("#VectorEnd1", {duration:0.5, alpha:0},1)
-//.from("#Vector3", {duration:2, drawSVG:0},1)
-//.from("#VectorEnd2", {duration:0.5, alpha:0},3)
-//;
-//return tl;
-//}
+function DrawLine(){
+    let tl = gsap.timeline({delay:0.5});
+tl.from("#VectorEnd1", {duration:0.25, alpha:0},"+=0.125")
+.from("#Vector3", {duration:1.75, drawSVG:0},"label")
+.to("#Ellipse3", {duration:1.5, drawSVG:0}, "label")
+.from("#VectorEnd2", {duration:0.125, alpha:0},"+=0.0125")
+;
+return tl;
+}
 
 function DrawCircles(){
     let tl = gsap.timeline();
-    tl.from("#VectorEnd1", {duration:0.25, alpha:0, ease:"none"})
-.from("#Vector3", {duration:2.5, drawSVG:0, ease:"none"},"label1")
-.from("#VectorEnd2", {duration:0.25, alpha:0, ease:"none"},"+=0.25")
-
-    .to("#Ellipse3", {duration:2, drawSVG:0}, "label1")
-    .from ("#cirlce2", {duration:1, scale:0}, "label")
-    .from("#button", {duration:2, y:-100, alpha:0, ease:"bounce.out"},"label")
+    tl
+    .from ("#cirlce2", {duration:0.75, scale:0},"label1")
+    .from("#button", {duration:0.75, y:-100, alpha:0, ease:"back.out"},"label1")
 ;
     return tl;
 }
 
 function Shutters(){
    let tl=gsap.timeline();
-   tl.to("#shutter1",{duration:1, fill:"#464846", x:"-=50", y:"-=70"})
-   .to("#shutter2",{duration:1, fill:"#464846", x:725, y:586})
+   tl.from("#shutter1",{duration:0.25, fill:"#464846", x:"+=100", y:"-=100", yoyo:true, repeat:3},"shutter")
+   .from("#shutter2",{duration:0.25, fill:"#464846", x:"-=100", y:"+=100", yoyo:true, repeat:3},"shutter")
    ;
    return tl;
     
 }
+function Reverse (){
+
+let tl=gsap.timeline();
+
+tl
+.set("#VectorEnd2", {alpha:0})
+
+.to("#Vector3",{duration:1.75, drawSVG:-0},"label2")
+.set("#VectorEnd1", {alpha:0})
+
+.to("#Ellipse3", {duration:1.5, drawSVG:"100%"},"label2")
+
+return tl;
+
+}
+function FadeOut(){
+    let tl=gsap.timeline();
+    tl.to("#cirlce2", {duration:0.75, scale:0, alpha:0}, "label3")
+    .to("#button", {duration:0.75, y:100, scale:0, transformOrigin:"center", ease:"back.in", alpha:0}, "label3");
+    return tl;
+
+
+}
 
 
 MainTL
-//.add(DrawLine())
-.add(DrawCircles())
+.add(DrawLine())
+.add(DrawCircles(),"-=0.5")
 .add(Shutters())
+.add(Reverse())
+.add(FadeOut(),"-=1.75")
+
+.repeat(-1)
 ;
+//gsap.to("#Vector3", {duration: 2, rotation: 30, ease: "easeOut"});
 
-
+//CustomWiggle.create("funWiggle", {wiggles: 10, type: "anticipate"});
 
 //function DrawLine(){
  //   let tl = gsap.timeline();
